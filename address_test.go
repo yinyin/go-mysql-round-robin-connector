d9bdaf50bd9d8981e830857208dcc86ac3387b0a
Yinyin L <yinyinl@gmail.com>
2020-02-09 19:39:00 +0800

    test parsing result caching

--- address_test.go
+++ address_test.go
@@ -68,3 +68,13 @@ func TestParseAddress03c(t *testing.T) {
 		t.Errorf("unexpect shuffle count: %d", r.shuffleCount)
 	}
 }
+
+func TestCacheParsedAddress(t *testing.T) {
+	r := parseAddress("loc-c/9/11")
+	if (r.locationName != "loc-c") || (r.orderedCount != 9) || (r.shuffleCount != 11) {
+		t.Errorf("unexpect result: (name: %s, ordered: %d, shuffle: %d)", r.locationName, r.orderedCount, r.shuffleCount)
+	}
+	if r2 := checkAddrCache("loc-c/9/11"); (r2 == nil) || (r.locationName != "loc-c") || (r.orderedCount != 9) || (r.shuffleCount != 11) {
+		t.Errorf("unexpect caching result: %#v", r2)
+	}
+}
